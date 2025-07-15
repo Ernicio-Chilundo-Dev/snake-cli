54a3c254347005ec07822e4867d48bf1d91007e9
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-15 20:18:46 +0200

    feat: implement Draw() function to render game screen and snake state using ncurses
    - Draws game borders using '#' characters
    - Displays snake head as '*', tail segments as '*', and food as '@'
    - Updates the screen with current snake position and food location
    - Shows the current score below the game board
    - Uses mvprintw() and refresh() from ncurses for real-time terminal rendering

--- snake.c
+++ snake.c
@@ -6,7 +6,7 @@
 #define HEIGHT 20
 #define WIDTH 20
 
-int x, y,foodx, foody, score;
+int x, y,foodX, foodY, score;
 int tailX[100], tailY[100];
 int nTail;
 int gamaOver;
@@ -28,12 +28,48 @@ void Setup(){
     x = WIDTH / 2;
     y = HEIGHT / 2;
     srand(time(0));
-    foodx = rand() % WIDTH;
-    foodx = rand() % HEIGHT;
+    foodX = rand() % WIDTH;
+    foodY = rand() % HEIGHT;
     score = 0;
 
 }
 
-// void Draw(){
-    
-// }
+void Draw()
+{
+    for (int i = 0; i < WIDTH + 2; i++)
+        mvprintw(0, i, "#");
+
+    // Desenha área do jogo
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (j == 0)
+                mvprintw(i + 1, j, "#");
+
+            if (i == y && j == x)
+                mvprintw(i + 1, j + 1, "*"); // Cabeça da cobra
+            else if (i == foodY && j == foodX)
+                mvprintw(i + 1, j + 1, "@"); // Comida
+            else {
+                int printTail = 0;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        mvprintw(i + 1, j + 1, "*");
+                        printTail = 1;
+                    }
+                }
+                if (!printTail)
+                    mvprintw(i + 1, j + 1, " ");
+            }
+
+            if (j == WIDTH - 1)
+                mvprintw(i + 1, j + 2, "#");
+        }
+    }
+
+    // Desenha borda inferior
+    for (int i = 0; i < WIDTH + 2; i++)
+        mvprintw(HEIGHT + 1, i, "#");
+
+    mvprintw(HEIGHT + 3, 0, "Score: %d", score);
+    refresh();
+}
