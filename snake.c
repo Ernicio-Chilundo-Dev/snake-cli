79ec80a3c2e63cfb10a8967a9369cc80cc86d793
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-15 10:43:22 +0200

    initscr();             // Initializes the ncurses screen clear();               // Clears the screen noecho();              // Disables echoing of typed characters cbreak();              // Disables line buffering (makes input respond instantly) curs_set(false);       // Hides the cursor from the terminal keypad(stdscr, TRUE);  // Enables arrow keys and function keys nodelay(stdscr, TRUE); // Makes  non-blocking

--- snake.c
+++ snake.c
@@ -5,3 +5,35 @@
 
 #define HEIGHT 20
 #define WIDTH 20
+
+int x, y,foodx, foody, score;
+int tailX[100], tailY[100];
+int nTail;
+int gamaOver;
+
+enum Direction {STOP = 0, LEFT, RIGHT, UP, DOWN};
+enum Direction dir;
+
+void Setup(){
+    initscr();
+    clear();
+    noecho();
+    cbreak();
+    curs_set(false);
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+
+    gamaOver = 0;
+    dir = STOP;
+    x = WIDTH / 2;
+    y = HEIGHT / 2;
+    srand(time(0));
+    foodx = rand() % WIDTH;
+    foodx = rand() % HEIGHT;
+    score = 0;
+
+}
+
+// void Draw(){
+    
+// }
